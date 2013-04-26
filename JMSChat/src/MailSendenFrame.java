3672c9f3728f2e82dd606152cd7ea884fcc29985
Christian Maran <cmaran@student.tgm.ac.at>
2013-04-26 12:26:32 +0200

    Graphische Oberfläche für Mailbox und "Mails schreiben"

---
+++ JMSChat/src/MailSendenFrame.java
@@ -0,0 +1,28 @@
+package JMSChat;
+import javax.swing.*;
+import java.awt.*;
+import java.net.InetAddress;
+
+/**
+ * Diese Klasse erzeugt das Fenster und fuehrt dies in der main-Methode aus.
+ * 
+ * @author Markus Schulmeister
+ * @date 2013-04-12
+ */
+public class MailSendenFrame extends JFrame {
+    private MailSendenPanel mp1;
+	/**
+     * Der Konstruktor erzeugt das Fenster und legt bestimmte Eigenschaften dafür fest.
+     */
+	
+    public MailSendenFrame(String name, String pw, String url) {
+        mp1 = new MailSendenPanel(name, pw, url, this);
+    	Container c = getContentPane();
+        c.add (mp1);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize (400, 400); // Größe des Fensters
+        setVisible (true);  // setzt ob man das Fenster sieht
+        setTitle ("Neue Mail");    // der Titel für das Fenster wird gesetzt
+    }
+}
+        
